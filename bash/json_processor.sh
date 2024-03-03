663f9ee65c930d00a83599901a310b4100da39fe
SV <sasvestor@gmail.com>
2024-03-03 02:30:37 +0200

    init
    text to json logs parser
    # parsing some test's .logs output & convert it to the json format

---
+++ bash/json_processor.sh
@@ -0,0 +1,57 @@
+#!/bin/bash 
+
+# Check if output file path is provided as argument
+if [ $# -eq 0 ]; then
+  echo "Error: Please provide the path to input.txt file as an argument."
+  exit 1
+fi
+
+# Read the input file
+
+input_file="$1"
+tests_json=""
+
+# Process the text file line by line
+while IFS= read -r line; do
+  # Check for line containing test name & parse it
+if [[ $line == *"["*"]"* ]]; then
+testName="$(awk '/^\[/ {print $2,$3;}' <<< $line)"
+testName_json="\"testName\": \"$testName\","
+fi
+
+
+  # Check for line starting with "not" & parse it
+if [[ $line =~ ^not ]]; then
+  duration="$(awk '/^not ok/ {print $NF}' <<< $line)"
+  test_count="$(awk '/^not ok/ {print $3}' <<< $line)"
+  name="$(awk -F"$test_count  " '{print $2}' <<< $line | awk -F", $duration" '{print $1}')"
+  status="false"
+  test_json="{\"name\": \"$name\",\"status\": $status,\"duration\": \"$duration\"}"
+  tests_json="${tests_json:+$tests_json,}$test_json"
+fi
+
+  # Check for line starting with "ok" & parse it
+if [[ $line =~ ^ok ]]; then
+  duration="$(awk '/^ok/ {print $NF}' <<< $line)"
+  test_count="$(awk '/^ok/ {print $2}' <<< $line)"
+  name="$(awk -F"$test_count  " '{print $2}' <<< $line | awk -F", $duration" '{print $1}')"
+  status="true"
+  test_json="{\"name\": \"$name\",\"status\": $status,\"duration\": \"$duration\"}"
+  tests_json="${tests_json:+$tests_json,}$test_json"
+fi
+
+  # Check for line starting with digits & parse it
+if [[ $line =~ ^[0-9] ]]; then
+  duration="$(awk '{print $NF}' <<< $line)"
+  success="$(awk '{print $1}' <<< $line)"
+  failed="$(awk '{print $6}' <<< $line)"
+  rating="$(awk -F'rated as ' '{print $2}' <<< $line | awk -F'%,' '{print $1}')"
+  summary_json="\"summary\": {\"success\": $success,\"failed\": $failed,\"rating\": $rating,\"duration\": \"$duration\"}"
+fi
+
+  # Gluing the full .json object 
+full_json="{$testName_json\"tests\": [$tests_json],$summary_json}"
+
+done < $input_file
+  # Parsing glued .json object by using "jq" util from the current repo
+echo $full_json | ./jq . 
